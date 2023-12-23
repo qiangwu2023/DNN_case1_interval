2180cf3ebea97cc3c8ce2d642db510250f5906b3
qiangwu2023 <72489564+qiangwu2023@users.noreply.github.com>
2023-12-23 19:28:28 +0800

    Add files via upload

---
+++ Model_Additive/data_generator.py
@@ -0,0 +1,40 @@
+"""
+Created on Wednesday December 20 2023
+@author: qiangwu
+"""
+import numpy as np
+import numpy.random as ndm
+
+def uniform_data(n, u1, u2):
+    a = ndm.rand(n)
+    b = (u2 - u1) * a + u1
+    return b
+
+def generate_case_2(n, corr, Beta):
+    Z = ndm.binomial(1, 0.5, n)
+    mean = np.zeros(5)
+    cov = np.identity(5)*(1-corr) + np.ones((5, 5))*corr
+    # X~t(0,Sigma,5) with the degree of freedom is 5
+    def multivariatet(mu,Sigma,N,M):
+        d = len(Sigma)
+        g = np.tile(np.random.gamma(N/2,1/2,M),(d,1)).T
+        Z = np.random.multivariate_normal(np.zeros(d),Sigma,M)
+        return mu + Z/np.sqrt(g/N)
+    
+    X = multivariatet(mean,cov,5,n)
+    # Constrain X to [0,2]
+    X = np.clip(X, 0, 2)
+    # ====The intercept term is approximately equal to 0.88 (by running choose_intercept.py)=====
+    g_X = X[:,0]**2/2 + 2*np.log(X[:,1]+1)/5 + 3*np.sqrt(X[:,2])/10 + np.exp(X[:,3])/5 + X[:,4]**3/10 - 1.62
+    Y = ndm.rand(n)
+    T = (-5 * np.log(Y) * np.exp(-Z * Beta - g_X)) ** 2 
+    U = uniform_data(n, 0, 10)
+    De = (T <= U)
+    return {
+        'Z': np.array(Z, dtype='float32'),
+        'X': np.array(X, dtype='float32'),
+        'T': np.array(T, dtype='float32'),
+        'U': np.array(U, dtype='float32'),
+        'De': np.array(De, dtype='float32'),
+        'g_X': np.array(g_X, dtype='float32')
+    }
---
+++ Model_Additive/g_deep.py
@@ -0,0 +1,65 @@
+# %%
+import torch
+from torch import nn
+#%% ------------ Return g(X) --------------
+def g_D(train_data,X_test,Lambda_U,Beta,Beta0,n_layer,n_node,n_lr,n_epoch):
+    Z_train = torch.Tensor(train_data['Z'])
+    X_train = torch.Tensor(train_data['X'])
+    U_train = torch.Tensor(train_data['U'])
+    De_train = torch.Tensor(train_data['De'])
+    g_train_true = torch.Tensor(train_data['g_X'])
+    X_test = torch.Tensor(X_test)
+    Lambda_U = torch.Tensor(Lambda_U)
+    Beta0 = torch.Tensor(Beta0)
+    # -------------- Define network structure --------------
+    class DNNModel(torch.nn.Module):
+        def __init__(self):
+            super(DNNModel, self).__init__()
+            layers = []
+            layers.append(nn.Linear(5, n_node))
+            layers.append(nn.ReLU())
+            for i in range(n_layer):
+                layers.append(nn.Linear(n_node, n_node))
+                layers.append(nn.ReLU())
+            layers.append(nn.Linear(n_node, 1))
+            self.model = nn.Sequential(*layers)
+            
+        def forward(self, x):
+            y_pred = self.model(x)
+            return y_pred
+
+
+    # -------------- Preparation before training --------------
+    model = DNNModel()
+    optimizer = torch.optim.Adam(model.parameters(), lr=n_lr)
+
+
+    def my_loss(De, Z, Beta, Lambda_U, g_X):
+        # Ensure that the shape of g_X is consistent with that of De, Z, U
+        assert(len(g_X.shape) == len(De.shape))
+        assert(len(g_X.shape) == len(Z.shape))
+        assert(len(g_X.shape) == len(Lambda_U.shape))
+        Lam1 = Lambda_U * torch.exp(Z*Beta + g_X)
+        loss_fun = -torch.mean(De*torch.log(1-torch.exp(-Lam1)+1e-5) - (1-De)*Lam1)
+        return loss_fun
+
+
+    # ------------ training -----------------
+    for epoch in range(n_epoch):
+        pred_g_X = model(X_train)
+        loss = my_loss(De_train, Z_train, Beta0, Lambda_U, pred_g_X[:, 0])
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+    #%% ----------- complete training ------------
+    # forecast
+    g_train = model(X_train)
+    g_test = model(X_test)
+    g_train = g_train[:,0].detach().numpy()
+    g_test = g_test[:,0].detach().numpy()
+    return {
+        'g_train': g_train,
+        'g_test': g_test
+    }
