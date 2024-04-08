53f98956d700d74711ac1eec8970b69561a00e7f
qiangwu2023 <72489564+qiangwu2023@users.noreply.github.com>
2024-04-08 19:37:09 +0800

    Update iteration_deep.py

--- Model_Deep1/iteration_deep.py
+++ Model_Deep1/iteration_deep.py
@@ -9,19 +9,14 @@ def Est_deep(train_data,X_test,Beta,Beta0,n_layer,n_node,n_lr,n_epoch,nodevec,m,
     U_train = train_data['U']
     De_train = train_data['De']
     Beta0 = np.array([Beta0])
-    # Given an initial value c0, calculate an initial value of Lambda(U)
     Lambda_U = I_S(m,c0,U_train,nodevec)
     C_index = 0
     for loop in range(100):
-        # The initial value of Beta and Lambda(U) are given, g(X) needs to be estimated
         g_X = g_D(train_data,X_test,Lambda_U,Beta,Beta0,n_layer,n_node,n_lr,n_epoch)
         g_train = g_X['g_train']
-        # Estimate Lambda(U)
         c1 = C_est(m,U_train,De_train,Z_train,Beta0,g_train,nodevec)
         Lambda_U = I_S(m,c1,U_train,nodevec)
-        # Estimate Beta
         Beta1 = Beta_est(De_train,Z_train,Lambda_U,g_train)
-        # Convergence condition
         if (abs(Beta0-Beta1) <= 0.001):
             C_index = 1
             break
