c7ddc0128f89016af2424d1a5628846c953cf764
qiangwu2023 <72489564+qiangwu2023@users.noreply.github.com>
2024-04-08 19:05:15 +0800

    Update iteration_linear.py

--- Model_Deep1/iteration_linear.py
+++ Model_Deep1/iteration_linear.py
@@ -7,18 +7,14 @@ def Est_linear(train_data,X_test,Beta0,nodevec,m,c0):
     U_train = train_data['U']
     De_train = train_data['De']
     Beta0 = np.array([Beta0])
-    # Given an initial value c0, calculate an initial value of Lambda(U)
     Lambda_U = I_S(m, c0, U_train, nodevec)
     C_index = 0
     for loop in range(100):
-        # The initial values of Lambda(U) is given,  Beta and g(X) need to be estimated
         g_X = g_L(train_data,X_test,Lambda_U)
         g_train = g_X['g_train']
         Beta1 = g_X['beta']
-        # Estimate Lambda(U)
         c1 = C_est(m,U_train,De_train,Z_train,Beta1,g_train,nodevec)
         Lambda_U = I_S(m,c1,U_train,nodevec)
-        # Convergence condition
         if (abs(Beta0-Beta1) <= 0.001):
             C_index = 1
             break
